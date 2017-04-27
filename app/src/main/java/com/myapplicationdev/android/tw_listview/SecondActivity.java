8c3caf415d10e82f45c3c8cb925b35ad9a0a90e4
ItsYaBoiBry <bryanlow987@gmail.com>
2017-04-27 13:10:37 +0800

    added the second activity added listview

--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -1,33 +1,44 @@
-    package com.myapplicationdev.android.tw_listview;
+package com.myapplicationdev.android.tw_listview;
 
-    import android.content.Intent;
-    import android.support.v7.app.AppCompatActivity;
-    import android.os.Bundle;
-    import android.widget.ListView;
-    import android.widget.TextView;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-    public class SecondActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-        ListView lv;
-        TextView tvYear;
+public class SecondActivity extends AppCompatActivity {
 
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_second);
-
-            lv = (ListView) this.findViewById(R.id.lvModules);
-            tvYear = (TextView) findViewById(R.id.tvYear);
-
-            Intent i = getIntent();
-            String year = i.getStringExtra("year");
-            tvYear.setText(year);
+    ListView lv;
+    TextView tvYear;
+    ArrayAdapter aa;
+    ArrayList<Module> module;
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
-        }
+        lv = (ListView) this.findViewById(R.id.lvModules);
+        tvYear = (TextView) findViewById(R.id.tvYear);
+        module = new ArrayList<Module>();
+        module.add(new Module("C208",false));
+        module.add(new Module("C200",true));
+        module.add(new Module("C346",false));
 
+        Intent i = getIntent();
+        String year = i.getStringExtra("year");
+        tvYear.setText(year);
+
+        aa = new ModuleAdapter(this,R.layout.activity_second_row, module);
+        lv.setAdapter(aa);
 
 
     }
+
+
+
+}
